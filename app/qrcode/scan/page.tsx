3e77a0d8dc9c3b194bd61a8ebb7938d1a9d13cae
Aryangp <guptaaryan0510@gmail.com>
2023-11-06 16:24:17 +0530

    changed the qr scan code

--- app/qrcode/scan/page.tsx
+++ app/qrcode/scan/page.tsx
@@ -1,15 +1,21 @@
-"use client";
 import QRCodeScanner from "@/app/components/QRCodeScanner";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const ScannerForQrCode = () => {
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
     return (
         <div className="flex justify-center">
             <div className="w-96 h-96 border-2 border-gray-300 rounded-lg overflow-hidden">
-                <QRCodeScanner onScan={(data) => console.log(data)} />
+                {isClient && <QRCodeScanner onScan={(data) => console.log(data)} />}
             </div>
         </div>
     );
 };
 
 export default ScannerForQrCode;
+
